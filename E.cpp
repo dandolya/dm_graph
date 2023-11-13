9c637387e713804242c8504f9aadd67e8f04d6a0
Daniil <117770118+dandolya@users.noreply.github.com>
2023-11-13 04:10:13 +0300

    Create E.cpp

---
+++ E.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <set>
+ 
+std::vector<std::vector<int>> matrix;
+ 
+int main()
+{
+    int n;
+    std::cin >> n;
+    matrix.resize(n + 1, std::vector<int>(0));
+    for (int i = 1; i < n; i++)
+    {
+        int a, b;
+        std::cin >> a >> b;
+        matrix[a].push_back(b);
+        matrix[b].push_back(a);
+    }
+    std::set<int> lists;
+    for (int i = 1; i <= n; i++)
+    {
+        if (matrix[i].size() == 1)
+        {
+            lists.insert(i);
+        }
+    }
+    for (int j = 0; j < n - 2; j++)
+    {
+                int i = *lists.begin();
+                std::cout << matrix[i][0] << " ";
+                //удалить ребро matrix[i][0] - matrix
+                lists.erase(lists.begin());
+                for (int k = 0; k < matrix[matrix[i][0]].size(); k++)
+                {
+                    if (matrix[matrix[i][0]][k] == i)
+                    {
+                        auto iter = matrix[matrix[i][0]].cbegin();
+                        matrix[matrix[i][0]].erase(iter + k);
+                        if (matrix[matrix[i][0]].size() == 1)
+                        {
+                            lists.insert(matrix[i][0]);
+                        }
+                        break;
+                    }
+                }
+                matrix[i].resize(0);
+ 
+    }
+}
